c690457b4f64a8916c90b1677c620dc510585cdc
MattGThomas <matthewgthomas0616@gmail.com>
2019-06-09 20:39:39 -0500

    changed name to birth year

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,7 +4,7 @@ const Character = props => {
     return (
         <div className='charContainer'>
             <h2>{props.char.name}</h2>
-            <p>Name: {props.char.birth_year}</p>
+            <p>Birth Year: {props.char.birth_year}</p>
             <p>Hair Color: {props.char.hair_color}</p>
             <p>Mass: {props.char.mass}</p>
             <p>Height: {props.char.height}</p>
